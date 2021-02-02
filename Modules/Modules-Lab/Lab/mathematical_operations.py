f8de873ebd5b5519315a32f71821e7a8948c742f
momchilantonov <eng.antonov@gmail.com>
2021-02-02 16:10:35 +0200

    Upload files

---
+++ Modules/Modules-Lab/Lab/mathematical_operations.py
@@ -0,0 +1,18 @@
+def add(n1, n2):
+    return n1+n2
+
+
+def subtract(n1, n2):
+    return n1-n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+def power(n1, n2):
+    return n1 ** n2
